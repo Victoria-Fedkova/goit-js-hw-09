b62e4ae2d8d17abf2f153409dcc6a7087f010578
Victoria-Fedkova <63882255+Victoria-Fedkova@users.noreply.github.com>
2023-03-18 23:24:58 +0000

    Deploying to gh-pages from @ Victoria-Fedkova/goit-js-hw-09@b5a058c916bddb026aed411597ae376970939343 🚀

---
+++ 01-color-switcher.4137f3ed.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){e.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(d)})),e.setAttribute("disabled",!0);let d=null;
+//# sourceMappingURL=01-color-switcher.4137f3ed.js.map
